d0bd81ef44e7b7d4ef563696076cba24bfef7dea
Vivek Prasad <vivekpsd01@gmail.com>
2026-08-16 14:01:53 +0530

    Data Types

--- JavaIntro/src/Basics.java
+++ JavaIntro/src/Basics.java
@@ -1,5 +1,30 @@
 public class Basics {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        System.out.println("Data Types:");
+        dataTypes();
+    }
+
+    public static void dataTypes() {
+        int num = 9;
+        byte by = 127;
+        short sh = 558;
+        long lg = 5854L;
+
+        float ft = 5.8f;
+        double d = 5.8;
+
+        char c = 'k';
+        boolean b = true;
+
+        System.out.println(num);
+        System.out.println(by);
+        System.out.println(sh);
+        System.out.println(lg);
+        System.out.println(ft);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(b);
     }
 }
