23f27df22ed9055eaf406f8771444fd0f993a8d4
Vivek Prasad <Vivekpsd01@gmail.com>
2026-09-14 00:38:22 +0530

    Array Basics

---
+++ Array/Basics.java
@@ -0,0 +1,50 @@
+package Array;
+
+import java.util.Arrays;
+
+public class Basics {
+    // implement single/multi-dimensional array, print them
+    // assign 2d array with random values
+    // implement enhanced for loop
+    public static void main(String[] args) {
+        // single dimensional array
+        
+        // with fixed size
+        int[] arr = new int[5];
+
+        // with variable size
+        // int[] arr = new int[]; // ERROR - won't compile - java needs to know how much memory to assign
+
+        // change array size?
+        int[] arr2 = new int[5];
+        arr2 = new int[10]; // this creates a NEW array, doesn't resize the old one
+
+        // fill array with default value
+        Arrays.fill(arr2, 2);
+
+        // fill some array elements with default value
+        Arrays.fill(arr2, 2, 7, 4); // fills indices 2 to 6 (start inclusive, end exclusive) with value 2
+
+        // print array
+        for (int n: arr) {
+            System.out.print(n);
+        }
+        System.out.println();
+
+        for (int n: arr2) {
+            System.out.print(n);
+        }
+        System.out.println();
+
+        int[][] arr2d = new int[5][5];
+
+        for (int i = 0; i < arr2d.length; i++) {
+            for (int j = 0; j < arr2d.length; j++) {
+                arr2d[i][j] = (int)(Math.random()*10);
+                System.out.print(arr2d[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
